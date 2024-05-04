2da21900a3e2a417b603f746b10da6779b761dbc
hanseehhh <mrhns00@gmail.com>
2024-05-05 03:34:23 +0700

    80% NOMOR 2

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -9,7 +9,16 @@ const authenticationServices = require('./authentication-service');
  * @returns {object} Response object or pass an error to the next route
  */
 
-let attempts = 1;
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getDate()).padStart(2, '0');
+const hours = String(currentDate.getHours()).padStart(2, '0');
+const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+let attempts = 0;
 let lockedUntil = undefined;
 
 const maxCoba = 5;
@@ -18,22 +27,29 @@ const timeOut = 30 * 60 * 1000; // 30 menit dalam milidetik
 async function login(request, response, next) {
   const { email, password } = request.body;
 
-
   try {
 
     if (lockedUntil && lockedUntil > Date.now()) {
-      throw errorResponder(errorTypes.FORBIDDEN);
+      throw errorResponder(
+        errorTypes.INVALID_CREDENTIALS,
+        `[${year}-${month}-${day} ${hours}:${minutes}:${seconds}] User ${email} telah terkunci hingga 30 menit kemudian` 
+      );
     }
 
     if (lockedUntil && lockedUntil <= Date.now()) {
       attempts = 0;
       lockedUntil = undefined;
+      console.log(`[${year}-${month}-${day} ${hours}:${minutes}:${seconds}] 
+      User ${email} bisa login kembali karena sudah melewati batas login}`)
     }
 
     if (attempts >= maxCoba) {
       if (!lockedUntil) {
         lockedUntil = Date.now() + timeOut;
-        throw errorResponder(errorTypes.FORBIDDEN);
+        throw errorResponder(
+          errorTypes.FORBIDDEN,
+          `[${year}-${month}-${day} ${hours}:${minutes}:${seconds}] User ${email} mencoba login, namun terdapat error 403 karena telah melebihi limit attempt`
+        );
       }
     }
     
@@ -48,13 +64,7 @@ async function login(request, response, next) {
       // Kembalikan pesan kesalahan jika mencoba login lebih dari 5 kali
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
-        'Wrong email or password'
-      );
-    }
-
-    if (attempts >= maxCoba) {
-      throw errorResponder(
-        errorTypes.AVOID_SPAM
+        `[${year}-${month}-${day} ${hours}:${minutes}:${seconds}] User ${email} gagal login. Attempt = ${attempts}`
       );
     }
 
