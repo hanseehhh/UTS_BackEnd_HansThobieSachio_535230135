8381703d725f3fc496de3fe1346aaa72d25160e2
hanseehhh <mrhns00@gmail.com>
2024-05-04 20:59:15 +0700

    fungsi masih error

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -11,32 +11,68 @@ const authenticationServices = require('./authentication-service');
 async function login(request, response, next) {
   const { email, password } = request.body;
 
+  const emailnya = await authenticationRepository.getUserByEmail(email);
+  const batascoba = 5; // Maximum login attempts allowed
+  const penalty = 30 * 60 * 1000; // Block duration in milliseconds (30 minutes)
+
+  let percobaan = {};
+  let lastLoginTime = 0;
+
   try {
-    // Check login credentials
+    const keblokir = loginAttempt.get(email);
+    
+    if(keblokir && keblokir.attempts >= batascoba){
+      const waktuDini = Date.now();
+
+      if(waktuDini - keblokir.lastAttemptTime  < penalty){
+        throw errorResponder(errorTypes.FORBIDDEN);
+      }
+
+      else{
+        loginAttempt.delete(email);
+      }
+    }
+
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
       password
     );
 
-    if (!loginSuccess) {
-      // throw errorResponder(
-      //   errorTypes.INVALID_CREDENTIALS,
-      //   'Wrong email or password'
-      // );
+ 
+    if(!loginSuccess){
+      if (attempts <= batascoba) {
+        if (loginSuccess) {
+          attempts = 0;
+          lastLoginTime = Date.now(); 
+        }
       
-      throw errorResponder(
-        errorTypes.FORBIDDEN,
-        'gagal login', [email],
-      );
+        else {
+          attempts ++;
+        }
+      }
+
+      if (attempts >= batascoba) {
+        throw errorResponder(
+          errorTypes.AVOID_SPAM
+        );
+      } 
+     
+      else {
+        attempts = 0;
+        return response.status(200).json(loginSuccess);
+      }
     }
 
-    
-  } catch (error) {
-    return next(error);
-  }
-}
+    attempts = 0;
+    lastLoginTime = Date.now(); 
+    return response.status(200).json(loginSuccess);
 
-module.exports = {
-  login,
-};
+  } 
+    catch (error) {
+    return next(error);
+    }
+}
+  module.exports = {
+    login,
+  };
 
